f0022372166f60e755f0e3e8f2f3bc8d69829e66
Paul <baoguok@gmail.com>
2025-12-20 18:43:41 +0800

    调整样式

--- views/default/index.php
+++ views/default/index.php
@@ -2,6 +2,7 @@
 
 use yii\jui\DatePicker;
 use jzkf\filemanager\models\File;
+use PhpOffice\PhpWord\Shared\Css;
 use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -31,7 +32,7 @@ $this->params['headerToolbar'][] = Html::button('<i class="ti ti-upload me-2"></
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col-auto">
-                            <span class="bg-primary text-white avatar"><i class="ti ti-files"></i></span>
+                            <span class="bg-primary text-white p-3 rounded"><i class="ti ti-files"></i></span>
                         </div>
                         <div class="col">
                             <div class="font-weight-medium">文件总数</div>
@@ -47,7 +48,7 @@ $this->params['headerToolbar'][] = Html::button('<i class="ti ti-upload me-2"></
                 <div class="card-body">
                     <div class="row align-items-center">
                         <div class="col-auto">
-                            <span class="bg-warning text-white avatar">
+                            <span class="bg-warning text-white p-3 rounded">
                                 <i class="ti ti-database"></i>
                             </span>
                         </div>
@@ -61,78 +62,74 @@ $this->params['headerToolbar'][] = Html::button('<i class="ti ti-upload me-2"></
         </div>
     </div>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); 
-    ?>
-
-    <div class="border rounded">
-        <div class="table-responsive">
-            <?= GridView::widget([
-                'dataProvider' => $dataProvider,
-                'filterModel' => $searchModel,
-                'tableOptions' => ['class' => 'table table-hover align-middle'],
-                'columns' => [
-                    'storage:raw',
-                    [
-                        'attribute' => 'origin_name',
-                        'format' => 'raw',
-                        'value' => function ($model) {
-                            return Html::a($model->origin_name, $model->base_url . $model->path, ['target' => '_blank']);
-                        }
-                    ],
-                    [
-                        'attribute' => 'path',
-                        'format' => 'raw',
-                        'value' => function ($model) {
-                            return Html::tag('div', Html::img($model->base_url . $model->path, ['class' => 'img-preview']), ['class' => 'img-preview-wrap']);
-                        }
-                    ],
-                    'mime_type',
-                    [
-                        'attribute' => 'size',
-                        'format' => 'raw',
-                        'value' => function ($model) {
-                            // $wh = '<br>W:' . $model->width . 'px H:' . $model->height . 'px';
-                            return format_bytes($model->size);
-                        }
-                    ],
-                    'extension',
-                    'upload_ip',
-                    [
-                        'attribute' => 'created_at',
-                        'format' => 'datetime',
-                        'filter' => DatePicker::widget([
-                            'model' => $searchModel,
-                            'attribute' => 'created_at',
-                            'options' => [
-                                'class' => 'form-control',
-                                'placeholder' => '选择日期',
-                                'autocomplete' => 'off',
-                            ],
-                            'clientOptions' => [
-                                'changeMonth' => true,
-                                'changeYear' => true,
-                                'yearRange' => '-10:+0',
-                                'showButtonPanel' => true,
-                                'closeText' => '确定',
-                                'currentText' => '今天',
-                                'dateFormat' => 'yy-mm-dd',
-                                'monthNames' => ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-                                'dayNames' => ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
-                                'dayNamesMin' => ['日', '一', '二', '三', '四', '五', '六'],
-                            ],
-                        ]),
-                    ],
-                    [
-                        'class' => 'yii\grid\ActionColumn',
-                        'template' => '<div class="d-flex shrink-0 items-center gap-3 justify-end">{view}{delete}</div>',
-                        'contentOptions' => [],
-                        'urlCreator' => function ($action, File $model, $key, $index, $column) {
-                            return Url::toRoute([$action, 'id' => $model->id]);
-                        }
-                    ],
+    <div class="table-responsive">
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'layout' => '{items}{summary}{pager}',
+            'tableOptions' => ['class' => 'table align-middle mt-3'],
+            'columns' => [
+                'storage:raw',
+                [
+                    'attribute' => 'origin_name',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        return Html::a($model->origin_name, $model->base_url . $model->path, ['target' => '_blank']);
+                    }
                 ],
-            ]); ?>
-        </div>
+                [
+                    'attribute' => 'path',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        return Html::tag('div', Html::img($model->base_url . $model->path, ['class' => 'img-preview']), ['class' => 'img-preview-wrap']);
+                    }
+                ],
+                'mime_type',
+                [
+                    'attribute' => 'size',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        // $wh = '<br>W:' . $model->width . 'px H:' . $model->height . 'px';
+                        return format_bytes($model->size);
+                    }
+                ],
+                'extension',
+                'upload_ip',
+                [
+                    'attribute' => 'created_at',
+                    'format' => 'datetime',
+                    'filter' => DatePicker::widget([
+                        'model' => $searchModel,
+                        'attribute' => 'created_at',
+                        'options' => [
+                            'class' => 'form-control',
+                            'placeholder' => '选择日期',
+                            'autocomplete' => 'off',
+                        ],
+                        'clientOptions' => [
+                            'changeMonth' => true,
+                            'changeYear' => true,
+                            'yearRange' => '-10:+0',
+                            'showButtonPanel' => true,
+                            'closeText' => '确定',
+                            'currentText' => '今天',
+                            'dateFormat' => 'yy-mm-dd',
+                            'monthNames' => ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+                            'dayNames' => ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+                            'dayNamesMin' => ['日', '一', '二', '三', '四', '五', '六'],
+                        ],
+                    ]),
+                ],
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '<div class="d-flex shrink-0 items-center gap-3 justify-end">{view}{delete}</div>',
+                    'contentOptions' => [],
+                    'urlCreator' => function ($action, File $model, $key, $index, $column) {
+                        return Url::toRoute([$action, 'id' => $model->id]);
+                    }
+                ],
+            ],
+        ]); ?>
     </div>
 
 </div>
